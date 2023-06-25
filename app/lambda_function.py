c9f5a6e6dece0aef0434363059fd9f5ecad9792c
Alan <alanfernando2809@gmail.com>
2023-06-25 15:33:27 -0300

    teste soma

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,2 +1,8 @@
+from app.src.soma import soma
+
+
 def lambda_handler(event, context):
-    return event["body"]
+    body = event["body"]
+    num1 = body["num1"]
+    num2 = body["num2"]
+    return soma(num1=num1, num2=num2)
