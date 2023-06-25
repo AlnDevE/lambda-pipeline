5e180b44f0c2902661796dfea102450bba731cad
Alan <alanfernando2809@gmail.com>
2023-06-25 15:18:27 -0300

    fix bug json

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,7 +1,9 @@
 from app.src.soma import soma
+import json
 
 
 def lambda_handler(event, context):
-    num1 = event["body"]["num1"]
-    num2 = event["body"]["num2"]
+    body = json.loads(event["body"])
+    num1 = body["num1"]
+    num2 = body["num2"]
     return soma(num1=num1, num2=num2)
