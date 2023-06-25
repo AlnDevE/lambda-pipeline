6586ced632dd519ae946810da0286b8197f4f5d6
Alan <alanfernando2809@gmail.com>
2023-06-25 15:26:28 -0300

    loading json

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -3,7 +3,7 @@ import json
 
 
 def lambda_handler(event, context):
-    body = json.loads(event["body"])
+    body = json.loads(event)["body"]
     num1 = body["num1"]
     num2 = body["num2"]
     return soma(num1=num1, num2=num2)
