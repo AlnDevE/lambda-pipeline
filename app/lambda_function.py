113b4ef16380dc29696e54db7b30964b2b6533ad
Alan <alanfernando2809@gmail.com>
2023-06-25 15:29:55 -0300

    test

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,9 +1,5 @@
-from app.src.soma import soma
 import json
 
 
 def lambda_handler(event, context):
-    body = json.loads(event)["body"]
-    num1 = body["num1"]
-    num2 = body["num2"]
-    return soma(num1=num1, num2=num2)
+    return json.loads(event)
